f47c18fd3c26a6a3c6323bcdca81cab9f8d98882
AChristoff <aleksov101@gmail.com>
2019-04-08 13:25:49 +0300

    Navbar pinned to top on scroll

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 function attachEvents() {
-    console.log('hi there');
+
+    //---------------------------------- Fixed nav
+
+    const nav = document.querySelector('#top');
+    const toOfNav = nav.offsetTop;
+    const navHeight = nav.offsetHeight;
+
+    function fixNav() {
+        if (window.scrollY > toOfNav + navHeight) {
+
+            $('#top').attr('class', 'row fixed-nav');
+            document.body.style.paddingTop = navHeight + 'px';
+
+        } else {
+            $('#top').attr('class', 'row');
+            document.body.style.paddingTop = 0;
+        }
+    }
+
+    window.addEventListener('scroll', fixNav);
+
+    //----------------------------------
+
+    $('#cv').on('click', () => {
+        console.log('downloading CV');
+    });
+
+    $('#contacts').on('click', () => {
+        console.log('contacts');
+    });
 }
+
