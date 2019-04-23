9a129030f1d7ade104c62f0fd4c8d1d69121a0f1
AChristoff <aleksov101@gmail.com>
2019-04-23 09:33:50 +0300

    Updates

---
+++ scripts/controllers/user-controller.js
@@ -0,0 +1,91 @@
+// REGISTER
+
+controllers.getRegister = function (context) {
+
+    context.loadPartials({
+        header: './views/common/header.hbs',
+        footer: './views/common/footer.hbs'
+    }).then(function () {
+        this.partial('./views/user/register.hbs');
+    }).catch((err) => console.log(err));
+};
+
+
+controllers.postRegister = function (context) {
+    let username = context.params.username;
+    let password = context.params.password;
+    let repeatPassword = context.params.repeatPassword;
+    $('#registerUsername').attr('class', 'form-control');
+
+    if (username === '') {
+        $('#registerUsername').attr('class', 'form-control is-invalid');
+        return notify.showError('Username is required!');
+    } else if (password === '') {
+        $('#registerPassword').attr('class', 'form-control is-invalid');
+        return notify.showError('Password is required!');
+    } else if (password !== repeatPassword) {
+        $('#registerPassword').attr('class', 'form-control is-invalid');
+        $('#registerRepeatPassword').attr('class', 'form-control is-invalid');
+        return notify.showError('Passwords don`t match!');
+    }
+
+    userService
+        .register(username, password)
+        .then((res) => {
+            userService.saveSession(res);
+            context.redirect('#/home');
+            notify.showSuccess('Registration is successful');
+        }).catch((err) => {
+            notify.handleError(err);
+    });
+};
+
+// LOGIN
+
+controllers.getLogin = function (context) {
+    context.loadPartials({
+        header: './views/common/header.hbs',
+        footer: './views/common/footer.hbs'
+    }).then(function () {
+        this.partial('./views/user/login.hbs')
+    }).catch((err) => console.log(err));
+};
+
+controllers.postLogin = function (context) {
+    console.log('click');
+    let username = context.params.username;
+    let password = context.params.password;
+    userService
+        .login(username, password)
+        .then((res) => {
+            userService.saveSession(res);
+            context.redirect('#/home');
+            notify.showSuccess('Login successful');
+        }).catch((err) => {
+
+        let loginUsername = $('#loginUsername');
+        let loginPassword = $('#loginPassword');
+
+        if (loginUsername.val() === '') {
+            loginUsername.attr('class', 'form-control is-invalid');
+            return notify.showError('Username is required!');
+        } else if (loginPassword.val() === '') {
+            loginPassword.attr('class', 'form-control is-invalid');
+            return notify.showError('Password is required!');
+        }
+
+        notify.handleError(err);
+    });
+};
+
+// LOGOUT
+
+controllers.getLogout = function (context) {
+    userService
+        .logout()
+        .then(() => {
+            sessionStorage.clear();
+            context.redirect('#/home');
+            notify.showSuccess('Logout successful');
+        }).catch((err) => console.log(err));
+};
---
+++ scripts/events.js
@@ -0,0 +1,71 @@
+function registerValidation() {
+    let regUsername = $('#registerUsername');
+    let regPassword = $('#registerPassword');
+    let regPasswordRepeat = $('#registerRepeatPassword');
+
+    regUsername.on('keyup', () => {
+        if (regUsername.val() !== '') {
+            regUsername.attr('class', 'form-control');
+        }
+    });
+
+    regPassword.on('keyup', () => {
+
+        if (regPassword.attr('class', 'form-control is-invalid')
+            && regPasswordRepeat.val() === '') {
+            regPassword.attr('class', 'form-control')
+        }
+
+        if (regPasswordRepeat.val() !== '') {
+            if (regPassword.val() === regPasswordRepeat.val()) {
+                regPassword.attr('class', 'form-control is-valid');
+                regPasswordRepeat.attr('class', 'form-control is-valid');
+            } else {
+                regPassword.attr('class', 'form-control is-invalid');
+                regPasswordRepeat.attr('class', 'form-control is-invalid');
+            }
+        }
+    });
+
+    regPasswordRepeat.on('keyup', () => {
+        if (regPassword.val() === regPasswordRepeat.val()) {
+            regPassword.attr('class', 'form-control is-valid');
+            regPasswordRepeat.attr('class', 'form-control is-valid');
+        } else {
+            regPassword.attr('class', 'form-control is-invalid');
+            regPasswordRepeat.attr('class', 'form-control is-invalid');
+        }
+    });
+}
+
+function loginValidation() {
+    let logUsername = $('#loginUsername');
+    let logPassword = $('#loginPassword');
+
+    logUsername.on('keyup', () => {
+        if (logUsername.val() !== '') {
+            logUsername.attr('class', 'form-control');
+        }
+    });
+
+    logPassword.on('keyup', () => {
+        if (logPassword.val() !== '') {
+            logPassword.attr('class', 'form-control');
+        }
+    });
+}
+
+function showCertificates() {
+
+    let showBtn = $('#showCertificates');
+    showBtn.on('click', () => {
+        let certificates = $('div#certificates');
+        if (!certificates.is(':visible')) {
+            certificates.css('display', 'flex');
+            showBtn.text('Hide certificates');
+        } else {
+            certificates.css('display', 'none');
+            showBtn.text('Show certificates');
+        }
+    });
+}
