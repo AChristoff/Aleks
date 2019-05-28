74b86eda47ed096e92ce1caf4c741c32937ce1f0
AChristoff <aleksov101@gmail.com>
2019-05-28 15:25:32 +0300

    update rain

--- scripts/utils/events.js
+++ scripts/utils/events.js
@@ -121,46 +121,42 @@ function m3x() {
     let c = document.getElementById("c");
     let ctx = c.getContext("2d");
 
-//making the canvas full screen
     c.height = window.innerHeight;
     c.width = window.innerWidth;
 
-//binary
     let binary = "1011010101010101010110101011101101011101001011";
-//converting the string into an array of single characters
+    setTimeout(() => {
+        binary = "⯌⯎✀✖✩✧✯✔✶*✎✤⯏⛏"
+    },1200);
+
     binary = binary.split("");
 
     let font_size = 12;
-    let columns = c.width / font_size; //number of columns for the rain
-//an array of drops - one per column
+    let columns = c.width / font_size;
+
     let drops = [];
-//x below is the x coordinate
-//1 = y co-ordinate of the drop(same for every drop initially)
+
     for (let x = 0; x < columns; x++)
         drops[x] = 0;
 
-//drawing the characters
+
     function draw() {
-        //Black BG for the canvas
-        //translucent BG to show trail
+
         ctx.fillStyle = "rgba(255, 255, 255, 0.1)";
         ctx.fillRect(0, 0, c.width, c.height);
 
-        ctx.fillStyle = "#dbe2e7"; //green text
+        ctx.fillStyle = "#dbe2e7";
         ctx.font = font_size + "px arial";
-        //looping over drops
+
         for (let i = 0; i < drops.length; i++) {
-            //a random chinese character to print
+
             let text = binary[Math.floor(Math.random() * binary.length)];
-            //x = i*font_size, y = value of drops[i]*font_size
+
             ctx.fillText(text, i * font_size, drops[i] * font_size);
 
-            //sending the drop back to the top randomly after it has crossed the screen
-            //adding a randomness to the reset to make the drops scattered on the Y axis
             if (c.height && Math.random() > 0.925)
                 drops[i] = 0;
 
-            //incrementing Y coordinate
             drops[i]++;
         }
     }
