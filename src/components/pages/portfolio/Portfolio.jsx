94212a614f217ee51e5ce269696563e1ad0490a4
aleksov <aleksov101@gmail.com>
2020-11-17 20:30:33 +0200

    add portfolio page

--- src/components/pages/portfolio/Portfolio.jsx
+++ src/components/pages/portfolio/Portfolio.jsx
@@ -1,36 +1,87 @@
 import './Portfolio.scss'
+import FlipCard from './partials/FlipCard'
 
 function Portfolio() {
   return (
     <div className="portfolio">
-      <h3>Portfolio</h3>
+      <h3 className="page-title">Portfolio</h3>
 
       <section className="card-list">
-        <div className="site-card">
-          <div className="card-head ">head</div>
-          <div className="card-body ">body</div>
-        </div>
+        <FlipCard
+          img="mern.png"
+          title="Fullstack JavaScript"
+          description="ReactJS, MongoDB, NodeJS, Express"
+          buttons={[
+            {
+              link: 'https://prodavalnik.aleksov.work/',
+              title: 'View project',
+              class: 'view-btn',
+            },
+            {
+              link: 'https://github.com/AChristoff/prodavalnik/',
+              title: 'Repo',
+              class: 'repo-btn',
+            },
+            {
+              link: 'https://prodavalnik-api.aleksov.work/api/docs/',
+              title: 'API',
+              class: 'api-btn',
+            },
+          ]}
+        />
 
-        <div className="site-card">
-          <div className="card-head ">head</div>
-          <div className="card-body ">body</div>
-        </div>
+        <FlipCard
+          img="casita-logo.png"
+          title="Business Website"
+          description="Vue.js & Sass"
+          buttons={[
+            {
+              link: 'https://casita-bg.com/',
+              title: 'View project',
+              class: 'view-btn',
+            },
+          ]}
+        />
 
-        <div className="site-card">
-          <div className="card-head ">head</div>
-          <div className="card-body ">body</div>
-        </div>
+        <FlipCard
+          img="nete-logo.png"
+          title="NETERRA Landing page"
+          description="JavaScript, HTML, CSS"
+          buttons={[
+            {
+              link: 'https://neterra.net/business-services/',
+              title: 'View project',
+              class: 'view-btn',
+            },
+          ]}
+        />
 
-        <div className="site-card">
-          <div className="card-head ">head</div>
-          <div className="card-body ">body</div>
-        </div>
+        <FlipCard
+          img="wordpress.png"
+          title="WP Custom theme"
+          description="Wordpress & Sass"
+          buttons={[
+            {
+              link: 'http://custom-wp-theme.fast-page.org/',
+              title: 'View project',
+              class: 'view-btn',
+            },
+          ]}
+        />
 
-        <div className="site-card">
-          <div className="card-head ">head</div>
-          <div className="card-body ">body</div>
-        </div>
-        
+        <FlipCard
+          img="ecta-logo.png"
+          title="ECTA 2020 landing page"
+          description="JavaScript, HTML, CSS"
+          buttons={[
+            {
+              link: 'https://www.ectacountryfocus.eu/',
+              title: 'View project',
+              class: 'view-btn',
+            },
+          ]}
+        />
+
       </section>
     </div>
   );
---
+++ src/components/pages/portfolio/partials/FlipCard.jsx
@@ -0,0 +1,33 @@
+import './FlipCard.scss'
+
+function FlipCard({img, title, description, buttons = []}) {
+  return (
+    <div className="flip-card">
+      <div className="flip-card-inner">
+        
+        <div className="flip-card-front">
+          <img id="cover" src={`/assets/img/cards/${img}`} alt={img} />
+        </div>
+        
+        <div className="flip-card-back">
+          
+          <h4 className="title">{title}</h4>
+          
+          <p className="description">{description}</p>
+
+          <section>
+          {buttons.map((button, index) => (
+            <a key={index} type="button" className={`card-btn ${button.class}`} href={button.link} target="_blank" rel="noreferrer">
+              {button.title}
+            </a>
+          ))}
+          </section>
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default FlipCard;
